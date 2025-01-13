718dafa3d2d9b94d831f9a890ec9d0286bcd407d
SadiaMs <162547312+SadiaMs@users.noreply.github.com>
2025-01-13 16:31:50 +0500

    Update page.tsx

--- src/app/getin/page.tsx
+++ src/app/getin/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Contact from '../components/Contact'
+import Image from 'next/image'
 
 const page = () => {
   return (
@@ -7,13 +8,13 @@ const page = () => {
 
 <div className="flex flex-col items-center mb-10">
           <div className="w-40% h-40% flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-            <img
+            <Image height={400} width={400}
               alt="dp"
               className="object-cover object-center h-[100%] w-full"
               src="/dp.png"
             />
           </div>
-          <h1>Lets Work Together ! </h1>
+          <h1>Lets Work Together  </h1>
         </div>
       <Contact />
     </div>
